547d12b715d0bfd6292640c13bdac68a456f640a
ZuraStore <faridgamingpro222@gmail.com>
2026-01-31 10:51:38 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "zura-bot-unified",
+  "version": "1.0.0",
+  "description": "Unified MNIT Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "npx playwright install chromium && node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "cheerio": "^1.0.0-rc.12",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "node-telegram-bot-api": "^0.64.0",
+    "playwright": "^1.40.0"
+  }
+}
+
+
